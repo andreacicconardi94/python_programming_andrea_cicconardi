6e163d02101ecb721adcb5555d8731ce48b208a4
andreacicconardi <andrea.cicconardi@studio.unibo.it>
2018-12-04 18:06:33 +0100

    Added Ex_1

---
+++ Exercise_1.py
@@ -0,0 +1,71 @@
+# Excercise 1
+#By Andrea Cicconardi
+
+#1
+import math
+A = 7.0
+print (A)
+
+#2
+B = 3.14
+
+#3
+C = A%2.0
+if C == 0:
+	print "A is an even number"
+else:
+	print "A is an odd number"
+
+#4
+D = (A+B)/2
+
+#5
+E = math.sqrt((A-D)**2)
+print E
+F = math.sqrt((B-D)**2)
+print F
+
+#6
+G = (E+F)/2
+
+#7
+A = A*2.0
+H = A-1
+
+#8
+C = A%2.0
+if C == 0:
+	print "A is an even number"
+else:
+	print "A is an odd number"
+
+D = (A+B)/2.0
+
+E = math.sqrt((A-D)**2)
+print E
+
+F = math.sqrt((B-D)**2)
+print F
+
+#9
+x1 = 4
+y1 = 8
+x2 = 15
+y2 = 16
+d = math.sqrt(((x1 - x2)**2) + ((y1 - y2)**2))
+
+#10
+P=0.7
+I = - (math.log2(P))
+
+#12
+x1 = 23
+y1 = 42
+x2 = 1
+y2 = 3
+d = math.sqrt(((x1 - x2)**2) + ((y1 - y2)**2))
+
+P=0.3
+I = - (math.log2(P))
+
+
